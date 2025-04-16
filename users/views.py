dfc86ff4fa98df3483fe68d1d6532e58e635712d
Maiadee <maiadeemmony@hotmail.co.uk>
2025-04-16 12:20:44 +0100

    added user serializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotAuthenticated, ValidationError
+from django.contrib.auth import get_user_model
+from .serializers.common import UserSerializer
+import jwt
+from django.conf import settings 
+from datetime import datetime, timedelta
+
+
 
 # Create your views here.
